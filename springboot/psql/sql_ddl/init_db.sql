67d06b96732b314b5d940c3e45d396f43eac7afd
danny-to7 <danny.to215@gmail.com>
2020-07-08 16:12:10 +0000

    Dockerfiles

--- springboot/psql/sql_ddl/init_db.sql
+++ springboot/psql/sql_ddl/init_db.sql
@@ -1,6 +1,10 @@
-CREATE DATABASE jrvstrading;
-GRANT ALL PRIVILEGES ON DATABASE jrvstrading TO postgres;
-
+CREATE DATABASE trading_dev;
+GRANT ALL PRIVILEGES ON DATABASE trading_dev TO postgres;
+\c trading_dev
+\i /docker-entrypoint-initdb.d/schema.sql
 --db for test case
-CREATE DATABASE jrvstrading_test;
-GRANT ALL PRIVILEGES ON DATABASE jrvstrading_test TO postgres;
+CREATE DATABASE trading_test;
+GRANT ALL PRIVILEGES ON DATABASE trading_test TO postgres;
+\c trading_test
+\i /docker-entrypoint-initdb.d/schema.sql
+
